330707e10a9ac2c1173fb23e2e766253bc4ae2b2
vadimoneplus <v.maranchak@gmail.com>
2023-07-13 18:44:25 +0300

    hw4 try2

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -21,7 +21,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         + ' '+(disabled
                 ? s.disabled
                 : xType === 'red'
-                    ?s.red:xType==='secondary'?s.secondary:''
+                    ?s.red:xType==='secondary'?s.secondary:s.default
         + (className ? ' ' + className : '') )// задачка на смешивание классов
 
     return (
